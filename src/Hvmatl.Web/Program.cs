0edd8b5eabcbe385ef9c6cd368f843aaa7f0fd3c
hiiamaalien@gmail.com <hiiamaalien@gmail.com>
2021-07-10 00:48:13 -0400

    Added Logging - Added Serilog - Update gitignore - Log are created in file, console, and database

--- src/Hvmatl.Web/Program.cs
+++ src/Hvmatl.Web/Program.cs
@@ -2,8 +2,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,16 +14,47 @@ namespace Hvmatl.Web
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; private set; }
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Build Configuration
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true,
+                     reloadOnChange: true)
+                .AddCommandLine(args)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // Configure serilog
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application Started");
+                CreateHostBuilder(args).Build().Run();
+                Log.Information("Application Ended");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Api host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            } 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseConfiguration(Configuration)
+                    .UseSerilog();
                 });
     }
 }
--- src/Hvmatl.Web/Startup.cs
+++ src/Hvmatl.Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Hvmatl.Web.Extensions;
+using Serilog;
 
 namespace Hvmatl.Web
 {
@@ -50,6 +51,8 @@ namespace Hvmatl.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSerilogRequestLogging();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
